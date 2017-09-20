04bbcb6ed1ded0774b2c1c2ef20f18d2c12ae35b
Patomphong Wongkalasin <patomphong.tho@gmail.com>
2017-09-20 21:03:34 +0700

    Set Input UI

--- ExchangeBalls/ExchangeBalls/ViewController.swift
+++ ExchangeBalls/ExchangeBalls/ViewController.swift
@@ -10,6 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //Outlet
+    @IBOutlet weak var textFieldInput: UITextField!
+    @IBOutlet weak var buttonAddInput: UIButton!
+    @IBOutlet weak var buttonReset: UIButton!
+    @IBOutlet weak var buttonExchange: UIButton!
+    @IBOutlet weak var textViewResult: UITextView!
+    @IBOutlet weak var labelErrorMessage: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +28,17 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func pressAddButton() {
+        print("Press Add Button")
+    }
+    
+    @IBAction func pressResetButton() {
+        print("Press Reset Button")
+    }
+    
+    @IBAction func pressExchangeButton() {
+        print("Press Exchange Button")
+    }
 
 }
 
