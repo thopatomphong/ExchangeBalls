1e17d7d40ffe0a492d4139fa93b90eb17d5cabf0
Patomphong Wongkalasin <patomphong.tho@gmail.com>
2017-09-20 20:26:13 +0700

    Function Ai And Bi Validation

--- ExchangeBalls/ExchangeBalls/Exchange.swift
+++ ExchangeBalls/ExchangeBalls/Exchange.swift
@@ -13,4 +13,8 @@ class Exchange {
     func validateOperationsAndSets(N:Int, K:Int) -> Bool {
         return (N >= 1 && N <= 50) && (K >= 1 && K <= Int(pow(Double(10), Double(9))))
     }
+    
+    func validateAiAndBi(_ Ai:Int, _ Bi:Int) -> Bool {
+        return ((Ai >= 1 && Ai <= 8) && (Bi >= 1 && Bi <= 8) && Ai != Bi)
+    }
 }
--- ExchangeBalls/ExchangeBallsTests/ExchangeBallsTests.swift
+++ ExchangeBalls/ExchangeBallsTests/ExchangeBallsTests.swift
@@ -13,29 +13,29 @@ class ExchangeBallsTests: XCTestCase {
     
     let exchange = Exchange()
     
-    func testValidateOperationsAndSetsWith4And2ShouldTrue() {
+    func testValidateOperationsAndSetsShouldTrue() {
         XCTAssertTrue(exchange.validateOperationsAndSets(N: 4, K: 2))
-    }
-    
-    func testValidateOperationsAndSetsWith16And1000000000ShouldTrue() {
         XCTAssertTrue(exchange.validateOperationsAndSets(N: 16, K: 1000000000))
     }
     
-    func testValidateOperationsAndSetsWith51And2ShouldFalse() {
+    func testValidateOperationsAndSetsShouldFalse() {
         XCTAssertFalse(exchange.validateOperationsAndSets(N: 51, K: 2))
-    }
-    
-    func testValidateOperationsAndSetsWith16And1000000001ShouldFalse() {
         XCTAssertFalse(exchange.validateOperationsAndSets(N: 16, K: 1000000001))
-    }
-    
-    func testValidateOperationsAndSetsWith0And1000000000ShouldFalse() {
         XCTAssertFalse(exchange.validateOperationsAndSets(N: 0, K: 1000000000))
-    }
-    
-    func testValidateOperationsAndSetsWith4And0ShouldFalse() {
         XCTAssertFalse(exchange.validateOperationsAndSets(N: 4, K: 0))
     }
     
+    func testValidateAiAndBiShouldReturnTrue() {
+        XCTAssertTrue(exchange.validateAiAndBi(1, 2))
+        XCTAssertTrue(exchange.validateAiAndBi(2, 1))
+        XCTAssertTrue(exchange.validateAiAndBi(2, 3))
+    }
+    
+    func testValidateAiAndBiShouldReturnFalse() {
+        XCTAssertFalse(exchange.validateAiAndBi(0, 8))
+        XCTAssertFalse(exchange.validateAiAndBi(9, 1))
+        XCTAssertFalse(exchange.validateAiAndBi(0, 7))
+        XCTAssertFalse(exchange.validateAiAndBi(4, 10))
+    }
 
 }
