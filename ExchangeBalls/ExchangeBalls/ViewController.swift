303029b6563d8a420f8c86bedc28254f693ad2f1
Patomphong Wongkalasin <patomphong.tho@gmail.com>
2017-09-20 21:15:11 +0700

    Integrated Function To UI

--- ExchangeBalls/ExchangeBalls/ViewController.swift
+++ ExchangeBalls/ExchangeBalls/ViewController.swift
@@ -18,6 +18,12 @@ class ViewController: UIViewController {
     @IBOutlet weak var textViewResult: UITextView!
     @IBOutlet weak var labelErrorMessage: UILabel!
     
+    //Parameter
+    var N:Int = 0
+    var K:Int = 0
+    var inputs = [(Ai:Int, Bi:Int)]()
+    var isShowResult:Bool = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -27,18 +33,116 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        addInputValues()
+        return true
+    }
 
     @IBAction func pressAddButton() {
-        print("Press Add Button")
+        addInputValues()
     }
     
     @IBAction func pressResetButton() {
-        print("Press Reset Button")
+        //Clear Display Values
+        textViewResult.text = ""
+        textFieldInput.text = ""
+        labelErrorMessage.text = ""
+        isShowResult = false
     }
     
     @IBAction func pressExchangeButton() {
-        print("Press Exchange Button")
+        if !isShowResult {
+            textFieldInput.text = ""
+            
+            let result = Exchange().exchangeBalls(N: N, K: K, inputs: inputs)
+            
+            textViewResult.text = textViewResult.text + "\n\n" + "Output: " + result
+            isShowResult = true
+        }
     }
 
+    func addInputValues() {
+        //Clear Error Message
+        labelErrorMessage.text = ""
+        
+        if isShowResult {
+            //Clear Result Text
+            textViewResult.text = ""
+            isShowResult = false
+            
+            //Reset Parameter
+            N = 0
+            K = 0
+            inputs = []
+        }
+        
+        let textInputs = textFieldInput.text
+        
+        guard let inputValues = textInputs?.components(separatedBy: " ") else {
+            return
+        }
+        
+        //check input space between values
+        if inputValues.count == 2 {
+            if N == 0 || K == 0 {
+                guard let inputN = Int(inputValues[0]), let inputK = Int(inputValues[1]) else {
+                    //Show Error Message
+                    labelErrorMessage.text = "Invalid Input : Please Input Again"
+                    return
+                }
+                
+                //Validate N and K Values
+                if Exchange().validateOperationsAndSets(N: inputN, K: inputK) {
+                    
+                    //Set Operations And Sets
+                    N = inputN
+                    K = inputK
+                    
+                    //Show Input
+                    textViewResult.text = "Input:" + "\n" + "\(N) \(K)"
+                } else {
+                    //Show Error Message
+                    labelErrorMessage.text = "Invalid Input : Please Input Again"
+                }
+            } else {
+                
+                guard let Ai = Int(inputValues[0]) else {
+                    //Show Error Message
+                    labelErrorMessage.text = "Invalid Input : Please Input Between 1 and 8"
+                    return
+                }
+                
+                guard let Bi = Int(inputValues[1]) else {
+                    //Show Error Message
+                    labelErrorMessage.text = "Invalid Input : Please Input Between 1 and 8"
+                    return
+                }
+                
+                //Validate Ai and Bi Values
+                if Exchange().validateAiAndBi(Ai, Bi) {
+                    
+                    //Save Input Values
+                    inputs.append((Ai, Bi))
+                    
+                    //Show Input Values
+                    textViewResult.text = textViewResult.text + "\n" + "\(Ai) \(Bi)"
+                } else {
+                    //Show Error Message
+                    labelErrorMessage.text = "Invalid Input : Please Input Values Again"
+                }
+            }
+            
+            textFieldInput.text = ""
+        } else {
+            //Show Error Message
+            labelErrorMessage.text = "Invalid Input : Please Input Space Between Values"
+        }
+    }
+    
+    func hideKeyboard() {
+        //Hide Keyboard
+        textFieldInput.resignFirstResponder()
+    }
 }
 
