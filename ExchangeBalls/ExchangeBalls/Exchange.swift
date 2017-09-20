2a52ee314e8cb819be75f748d48c25f1b6ce4b96
Patomphong Wongkalasin <patomphong.tho@gmail.com>
2017-09-20 20:40:46 +0700

    Function Swap Balls

--- ExchangeBalls/ExchangeBalls/Exchange.swift
+++ ExchangeBalls/ExchangeBalls/Exchange.swift
@@ -10,11 +10,19 @@ import Foundation
 
 class Exchange {
     
+    var ballNumbers : [Int] = [1, 2, 3, 4, 5, 6, 7, 8]
+    
     func validateOperationsAndSets(N:Int, K:Int) -> Bool {
         return (N >= 1 && N <= 50) && (K >= 1 && K <= Int(pow(Double(10), Double(9))))
     }
     
     func validateAiAndBi(_ Ai:Int, _ Bi:Int) -> Bool {
         return ((Ai >= 1 && Ai <= 8) && (Bi >= 1 && Bi <= 8) && Ai != Bi)
+    }
+    
+    func swapBalls(_ indexA:Int, _ indexB:Int) {
+        if validateAiAndBi(indexA, indexB) {
+            swap(&ballNumbers[indexA - 1], &ballNumbers[indexB - 1])
+        }
     }
 }
--- ExchangeBalls/ExchangeBallsTests/ExchangeBallsTests.swift
+++ ExchangeBalls/ExchangeBallsTests/ExchangeBallsTests.swift
@@ -13,6 +13,7 @@ class ExchangeBallsTests: XCTestCase {
     
     let exchange = Exchange()
     
+    //Test Validate Operations And Sets
     func testValidateOperationsAndSetsShouldTrue() {
         XCTAssertTrue(exchange.validateOperationsAndSets(N: 4, K: 2))
         XCTAssertTrue(exchange.validateOperationsAndSets(N: 16, K: 1000000000))
@@ -25,6 +26,7 @@ class ExchangeBallsTests: XCTestCase {
         XCTAssertFalse(exchange.validateOperationsAndSets(N: 4, K: 0))
     }
     
+    //Test Validate Ai And Bi
     func testValidateAiAndBiShouldReturnTrue() {
         XCTAssertTrue(exchange.validateAiAndBi(1, 2))
         XCTAssertTrue(exchange.validateAiAndBi(2, 1))
@@ -36,6 +38,31 @@ class ExchangeBallsTests: XCTestCase {
         XCTAssertFalse(exchange.validateAiAndBi(9, 1))
         XCTAssertFalse(exchange.validateAiAndBi(0, 7))
         XCTAssertFalse(exchange.validateAiAndBi(4, 10))
+    }
+    
+    //Test Swap Balls
+    func testSwapBallsShouldReturnTrue() {
+        
+        exchange.swapBalls(1, 2)
+        
+        XCTAssertEqual(exchange.ballNumbers[0], 2)
+        XCTAssertEqual(exchange.ballNumbers[1], 1)
+        
+        exchange.swapBalls(4, 5)
+        
+        XCTAssertEqual(exchange.ballNumbers[3], 5)
+        XCTAssertEqual(exchange.ballNumbers[4], 4)
+    }
+    
+    func testSwapBallsShouldReturnFalse() {
+        
+        exchange.swapBalls(9, 2)
+        
+        XCTAssertFalse(exchange.ballNumbers[1] == 9)
+        
+        exchange.swapBalls(5, 10)
+        
+        XCTAssertFalse(exchange.ballNumbers[4] == 10)
     }
 
 }
